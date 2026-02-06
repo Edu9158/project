e15e51c99f42ff3b1aaac426a38a9ac4b9d958cc
Eduardo de Moraes Marinho <eduardomoraes096@gmail.com>
2026-02-06 20:58:39 -0300

    feat: app.py flask app start

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from Flask import Flask
+import flask
 from routes.tracking import bp_tracking
 from routes.users import bp_users
 import os
 
 def create_app():
+    # creates flask app
     app = flask(__name__)
 
-    app.register_blueprint(bp_tracking, url_prefix='/api/v1/inventory')
-    app.register_blueprint(bp_users, url_prefix='/api/v1/users')
-    
+    # define tracking blueprints url
+    app.register_blueprint(bp_tracking, url_prefix = '/api/v1/inventory')
+    app.register_blueprint(bp_users, url_prefix = '/api/v1/users')
     return app
 
 if __name__ == "__main__":
+    # starts flask app
     app = create_app()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host = '0.0.0.0', port = port, debug = True)
